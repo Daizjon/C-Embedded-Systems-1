55a7df092c1e3b38d61d09b8ef989df43c6bcea5
Dai'zjon Carney <89041983+Daizjon@users.noreply.github.com>
2022-08-07 14:35:43 -0400

    Add files via upload

---
+++ IO, Interrupts, Stack Use, and Timing/Sources/main.c
@@ -0,0 +1,20 @@
+#include <MKL25Z4.H>
+#include "gpio_defs.h"
+#include "LEDs.h"
+#include "switches.h"
+
+extern volatile unsigned switch_pressed;
+extern void init_debug_signals(void);
+
+int main (void) {
+	
+	init_switch();
+	init_RGB_LEDs();
+	init_debug_signals();
+	__enable_irq();
+	
+	while (1) {
+		DEBUG_PORT->PTOR = MASK(DBG_MAIN_POS);
+		control_RGB_LEDs(count&1, count&2, count&4);
+	}
+}
